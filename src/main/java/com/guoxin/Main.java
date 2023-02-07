2df8a56e49847065cf538c3360c3fc7372a27a87
guoxin <605430640@qq.com>
2023-02-07 20:11:11 +0800

    idea

---
+++ src/main/java/com/guoxin/Main.java
@@ -0,0 +1,8 @@
+package com.guoxin;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("我的第一个git文件");
+    }
+}
