7780df885e1224f4e2eaa4057bb54270bc5e6e9e
guoxin <605430640@qq.com>
2023-02-07 20:15:25 +0800

    test idea

--- src/main/java/com/guoxin/Main.java
+++ src/main/java/com/guoxin/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("我的第一个git文件");
+        System.out.println("test");
     }
 }
